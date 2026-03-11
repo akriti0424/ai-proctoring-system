ef50f98d1e9e145d43a157409e67dda6484c1a5a
Akriti Sharma <akritisharma2344@gmail.com>
2026-03-12 01:19:51 +0530

    Created FastAPI application entry point

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,30 +1,27 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-
-#importing controllers
-from app.controllers.exam_controller import router as exam_router
 from app.controllers.auth_controller import router as auth_router
+from app.controllers.exam_controller import router as exam_router
 from app.controllers.admin_controller import router as admin_router
 
 app = FastAPI(title="AI Proctoring System")
 
+# Allow frontend requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-#including API routers
+# Include controllers
 app.include_router(auth_router)
 app.include_router(exam_router)
 app.include_router(admin_router)
 
 
-#Serving fontend static files
-app.mount("/static", StaticFiles(directory = "../frontend"), name = "static")
-
 @app.get("/")
 def home():
-    return FileResponse("../frontend/pages/login.html")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    return {"message": "AI Proctoring System Running"}
